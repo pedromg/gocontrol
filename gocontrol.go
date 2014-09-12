0b0eeb92a6567383c59878a4e9f1710d535546d0
pedro mg <pedro@tquadrado.com>
2014-09-12 06:28:07 +0100

    Test for JSON file added.

--- gocontrol.go
+++ gocontrol.go
@@ -26,7 +26,7 @@ func json_to_requestinfo(j []byte) (r []RequestInfo, err error) {
 	return r, err
 }
 
-func getRequestInfo(f string) (r []RequestInfo, err error) {
+func GetRequestInfo(f string) (r []RequestInfo, err error) {
 	var the_json []byte
 	the_json, err = ioutil.ReadFile(f)
 	if err == nil {
@@ -42,9 +42,12 @@ func main() {
 	json_file := "./files/gocontrol.json"
 	// get the requests to monitor
 
-	res, err := getRequestInfo(json_file)
+	res, err := GetRequestInfo(json_file)
 	if  err != nil {
 		log.Fatal("Fatal error loading the JSON data,", err)
+	}
+	for _, elem := range res {
+		fmt.Printf("URL: %s Script:%s (Secure:%v) \n", elem.Url, elem.Script, elem.Secure)
 	}
 
 	// create go routines for each request
--- gocontrol_test.go
+++ gocontrol_test.go
@@ -1,9 +1,34 @@
-package gocontrol
+package main
 
 import (
 	"testing"
 	"github.com/pedromg/gocontrol"
 )
+
+func TestInexistentJSONFile( t *testing.T) {
+	f := "./test/not_existing_agocontrol.json"
+	_, e := main.GetRequestInfo(f)
+	if e == nil {
+		t.Error("JSON File Load Error:", e)
+	}
+}
+func TestValidJSONFile( t *testing.T) {
+	f := "./test/gocontrol.json"
+	r, e := main.GetRequestInfo(f)
+	if e != nil {
+		t.Error("JSON File Load Error:", e)
+	}
+	if r[0].Url != "github.com" || r[1].Url != "google.com" {
+		t.Error("Unmarshaled JSON error")
+	}
+	if r[0].Secure != true || r[1].Secure != false {
+		t.Error("Unmarshaled JSON error")
+	}
+}
+
+func TestGetRequestInfo( t *testing.T) {
+	t.Log("are the arguments valid?")
+}
 
 func TestScriptExists( t *testing.T) {
 	t.Log("script exists ?")
@@ -17,6 +42,3 @@ func TestValidReply( t *testing.T) {
 	t.Log("is the reply covered ?")
 }
 
-func TestArgParser( t *testing.T) {
-	t.Log("are the arguments valid?")
-}
